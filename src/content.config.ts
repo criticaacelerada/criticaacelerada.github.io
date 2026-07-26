b799561b404150d6270943361ad01458d21c2a51
Samuel Carmona <samuelcarmona@MacBook-Air-de-Samuel.local>
2026-07-26 13:40:28 -0600

    Primer expediente publicado

---
+++ src/content.config.ts
@@ -0,0 +1,19 @@
+import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
+
+const ensayos = defineCollection({
+  loader: glob({
+    pattern: "**/*.md",
+    base: "./src/content/ensayos",
+  }),
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+    expediente: z.number(),
+    descripcion: z.string(),
+  }),
+});
+
+export const collections = {
+  ensayos,
+};
